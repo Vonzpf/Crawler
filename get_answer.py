c83485e0cb4b872db427b6e9614b6cfd741efef2
Vonzpf <eternityzpf@sina.com>
2016-07-30 16:08:42 +0800

    完善信息抓取

--- get_answer.py
+++ get_answer.py
@@ -2,33 +2,63 @@
 
 import urllib
 import re
+#import os
 from bs4 import BeautifulSoup
-from bs4.builder._htmlparser import BeautifulSoupHTMLParser
+#from bs4.builder._htmlparser import BeautifulSoupHTMLParser
 from docx import Document
-from docx.shared import Inches
+#from docx.shared import Inches
 
-def geturl(urllist):
-    answerpage = urllib.urlopen(urllist)
-    answerhtml = answerpage.read()
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getanswer(html):
+    reg = r'data-entry-url=".+"'
+    urlre = re.compile(reg)
+    urllist = re.findall(urlre,html)
+    x = 0
+    title_doc = Document()
+    for answerurl in urllist:
+        urlsplited = answerurl.split('"')
+        fullurl = 'https://www.zhihu.com' + urlsplited[1]   #获取完整的答案URL
         
-    soup = BeautifulSoup(answerhtml,"lxml")
-    a_tag = soup.find("div", class_="zm-editable-content clearfix")
-    while(a_tag.find_all("noscript")):
-        a_tag.noscript.extract()
-    while(a_tag.find_all("br")):
-        a_tag.br.decompose()
-    while(a_tag.find_all("b")):
-        a_tag.b.unwrap()
-    while(a_tag.find_all("u")):
-        a_tag.u.unwrap()
-    answer = a_tag.prettify()
-    #获取答案
-    #print(a_tag.original_encoding)
-    
-    document = Document()
-    document.add_paragraph(answer)
-    document.add_page_break()
-    document.save('d:\demo.docx')
-    
-urllist = "https://www.zhihu.com/question/30285168/answer/110433971"
-geturl(urllist)
+        answerpage = urllib.urlopen(fullurl)
+        answerhtml = answerpage.read()
+        
+        soup = BeautifulSoup(answerhtml,"lxml")
+        title_tag = soup.find("h2", class_="zm-item-title")
+        answer_tag = soup.find("div", class_="zm-editable-content clearfix")
+        
+        title_tag.a.unwrap()
+        
+        #处理回答中不必要的标签
+        while(answer_tag.find_all("noscript")):
+            answer_tag.noscript.extract()
+        while(answer_tag.find_all("br")):
+            answer_tag.br.decompose()
+        while(answer_tag.find_all("b")):
+            answer_tag.b.unwrap()
+        while(answer_tag.find_all("u")):
+            answer_tag.u.unwrap()
+        while(answer_tag.find_all("p")):
+            answer_tag.p.unwrap()
+        
+        title = unicode(x)+title_tag.get_text()
+        print(title)
+        
+        answer = answer_tag.prettify()
+        
+        #保存为word
+        document = Document()
+        document.add_paragraph(answer)
+        document.save('d:\%s.docx' %x)
+        
+        title_doc.add_paragraph(title)
+        x += 1
+
+    title_doc.save('d:\\alltitle.docx')
+
+html = getHtml("https://www.zhihu.com/collection/47588314?page=1")
+
+getanswer(html)
